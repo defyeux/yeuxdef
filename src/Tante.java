f538eae22f41dfcbc8ddf43053be4cbde46215b9
defyeux <230487@astanait.edu.kz>
2024-04-08 15:52:15 +0500

    yeux

--- src/Tante.java
+++ src/Tante.java
@@ -5,18 +5,36 @@ public class Tante {
         Scanner scan = new Scanner(System.in);
         System.out.println("Choose number 1-10");
         int ch = scan.nextInt();
+        int n;
+        int[] lst;
         switch (ch) {
             case 1:
                 System.out.print("Input n: ");
-                int n = scan.nextInt();
+                n = scan.nextInt();
                 System.out.print("Input an array of n elements: ");
-                int[] lst = new int[n];
+                lst = new int[n];
 
                 for (int i = 0; i < n; i++) {
                     lst[i] = scan.nextInt();
                 }
 
                 System.out.printf("The minimum of them: %d\n", first(n, lst));
+
+            case 2:
+                System.out.print("Input n: ");
+                n = scan.nextInt();
+                System.out.print("Input an array of n elements: ");
+                lst = new int[n];
+
+                for (int i = 0; i < n; i++) {
+                    lst[i] = scan.nextInt();
+                }
+
+                System.out.println("The average of given array: " + second(n, lst));
+            case 3:
+                System.out.print("Input integer number: ");
+                n = scan.nextInt();
+                System.out.printf("%d is %s\n", n, isPrime(n));
         }
     }
 
@@ -63,4 +81,23 @@ public class Tante {
      */
 
 
+    public static String isPrime(int n) {
+        for (int x = 2; x <= n / 2; x++) {
+            if (n % x == 0) {
+                return "Composite";
+            }
+        }
+
+        return "Prime";
+    }
+
+    /**
+     * This method determine if given number is prime or composite
+     * It uses iteration approach
+     * Time complexity: O(n), where n is the length of the array
+     * The iterative algorithm iterates through all numbers for 2 to n - 2
+     * resulting in linear time complexity
+     * @param n is a number that is checked on prime number
+     * @return this number is prime or composite
+     */
 }
