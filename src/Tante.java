c8a65a3fa78b86a7011067c85b2c38d149766595
defyeux <230487@astanait.edu.kz>
2024-04-08 17:27:26 +0500

    yeux

--- src/Tante.java
+++ src/Tante.java
@@ -34,11 +34,15 @@ public class Tante {
             case 3:
                 System.out.print("Input integer number: ");
                 n = scan.nextInt();
-                System.out.printf("%d is %s\n", n, isPrime(n));
+                System.out.printf("%d is %s\n", n, third(n));
+            case 4:
+                System.out.print("Input integer number: ");
+                n = scan.nextInt();
+                System.out.printf("Factorial of %d - %d", n, fourth(n));
         }
     }
 
-    public static int first(int n, int[] lst) {
+    private static int first(int n, int[] lst) {
         int min = lst[0];
 
         for (int i = 1; i < n; i++) {
@@ -60,7 +64,7 @@ public class Tante {
      * @return number is a minimun of given array
      */
 
-    public static double second(int n, int[] lst) {
+    private static double second(int n, int[] lst) {
         int sum = 0;
 
         for (int x: lst) {
@@ -81,7 +85,7 @@ public class Tante {
      */
 
 
-    public static String isPrime(int n) {
+    private static String third(int n) {
         for (int x = 2; x <= n / 2; x++) {
             if (n % x == 0) {
                 return "Composite";
@@ -99,5 +103,21 @@ public class Tante {
      * resulting in linear time complexity
      * @param n is a number that is checked on prime number
      * @return this number is prime or composite
+     */
+
+    private static int fourth(int n) {
+        if (n == 0 || n == 1) return 1;
+
+        return n * fourth(n - 1);
+    }
+
+    /**
+     * This method calculate factorial of a given number
+     * It uses recursive approach
+     * Time complexity: O(n), where n is the length of the array
+     * The recursive algorithm iterates through all numbers from n to 1
+     * resulting in linear time complexity
+     * @param n is a number that is to be found
+     * @return the factorial of given number
      */
 }
