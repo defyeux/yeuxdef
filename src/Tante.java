5f673389e351218f98523e4e30626e22c29fb5ba
defyeux <230487@astanait.edu.kz>
2024-04-08 15:23:16 +0500

    yeux

--- src/Tante.java
+++ src/Tante.java
@@ -35,11 +35,29 @@ public class Tante {
     /**
      * This method calculates the minimun of a given array of a given length
      * It uses iteration approach
-     * Time complexity: O(n), where n is the input number
+     * Time complexity: O(n), where n is the length of the array
      * The iterative algorithm iterates through all numbers from 1 to n - 1
      * resulting in linear time complexity
      * @param n is a length of array, @param lst is an array, whose minimun element is to be found
      * @return number is a minimun of given array
      */
 
+    public static double second(int n, int[] lst) {
+        int sum = 0;
+
+        for (int x: lst) {
+            sum += x;
+        }
+
+        return (double) sum / n;
+    }
+    /**
+     * This method calculates the average of a given array
+     * It uses iteration approach
+     * Time complexity: O(n), where n is the length of the array
+     * The iterative algorithm iterates through all numbers for 0 to n - 1
+     * resulting in linear time complexity
+     * @param n is a length of array, @param lst is an array, whose average is to be found
+     * @return average of a given array
+     */
 }
